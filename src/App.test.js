06a05ed703eeea85c9ce1e107e6ef29868cf8119
MrFuentastic <vfuentes8587@gmail.com>
2018-03-12 20:11:24 -0500

    WIP - added timetable, needs to be tested and put inside of a css grid

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import TimeTable from './TimeTable'
+import {shallow} from 'enzyme'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('TimeTable', () => {
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('renders the days without crashing', () => {
+    console.log('asdfasdfasdf');
+    ReactDOM.render(<TimeTable />, div )
+    expect(shallow(<TimeTable />, div)).exists(<TimeTable />).toBe(true);
+  });
+
 });
---
+++ src/TimeTable.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import Data from './data';
+
+let times = [
+  "8:00 AM",
+  "9:00 AM",
+  "10:00 AM",
+  "11:00 AM",
+  "12:00 PM",
+  "1:00 PM",
+  "2:00 PM",
+  "3:00 PM",
+  "4:00 PM",
+  "5:00 PM"
+  ];
+
+
+function Day(props){
+  // render() {
+
+    return (
+      <div className="time-table-column">{props.name}
+        <select id={props.name} onChange={(e) => chooseTime(props.name, e)}>
+          {
+          times.map((time, index) => {
+              return (
+                <option key={index}>{time}</option>
+              );
+            })
+          }
+        </select>
+      </div>
+    );
+  // }
+}
+
+function chooseTime(name) {
+  var el = document.getElementById(name);
+  var value = el.options[el.selectedIndex].value;
+  console.log(name, value);
+}
+
+class TimeTable extends Component {
+  render() {
+    return (
+      <div className="component container">
+        <div>
+            {Data.data.map((item, index) => {
+               return <Day key={index} name={item.date} />
+            })}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default TimeTable;
