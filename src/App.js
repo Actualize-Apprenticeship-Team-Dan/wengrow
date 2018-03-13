3a5e1daf182260ce8cbead60c2dbd47791b859b1
heydanhey <danhey@gmail.com>
2018-03-13 08:24:59 -0500

    example

--- src/App.js
+++ src/App.js
@@ -1,17 +1,45 @@
 import React, { Component } from 'react';
 import './App.css';
-// import data from './data.json';
-import TimeTable from './TimeTable';
+import firebase from './firebase.js';
 
 class App extends Component {
+   constructor(props) {
+    super(props);
+    this.state = {
+      dates: [
+        "9:00am - 10:00am",
+        "10:00am - 11:00am",
+        "11:00am - 12:00pm",
+        "12:00pm - 1:00pm",
+        "1:00pm - 2:00pm",
+        "2:00pm - 3:00pm",
+        "3:00pm - 4:00pm",
+        "4:00pm - 5:00pm" 
+      ]
+    };
+
+  }
+
+  handleClick() {
+    const datesRef = firebase.database().ref('dates');
+    let dateSelect = document.getElementById('dateSelect');
+    let value = dateSelect.options[dateSelect.selectedIndex].value;
+    datesRef.push(value);
+  }
+
   render() {
     const title = 'Wengrow\'s Appointments'
     return (
-      <div className="App">
-        <h1>
-        {title}
-        </h1>
-      <TimeTable/>
+      <div class="App">
+        <h1> Available Dates </h1>
+        <select id="dateSelect">
+        {
+          this.state.dates.map(function(item, i){
+            return <option key={i} value={item}>{item}</option>
+          })
+        }
+        </select>
+        <button onClick={this.handleClick}> Submit </button>
       </div>
     );
   }
